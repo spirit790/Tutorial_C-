41aaeaaa8532837fe7ec49e25be465befb34b11b
spirit790 <rkdqhdrnt@naver.com>
2023-04-25 12:41:22 +0900

    1
    1

---
+++ HellowWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() //unity에서 start와 비슷한 함수
+{
+	printf("Hello World\n\n");//\n은 엔터키
+	printf("%d\n\n", 1234);//기본적으로 printf는 문자열로 출력1234를 정수로 출력하기위해서 씀 는 어짜피 비슷함
+
+	printf("나는 %d살 입니다\n\n", 20);
+
+	return 0;
+}
